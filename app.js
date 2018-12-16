e65618ba5d49b0ba688d8739b87305fe900c67a7
Vishesh <visheshsingh16@gmail.com>
2018-12-16 15:29:44 -0500

    Getting data from Firestore

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+// Cafe List
+const cafeList = document.querySelector("#cafe-list");
+
+function render(doc) {
+  const li = document.createElement("li");
+  const name = document.createElement("span");
+  const city = document.createElement("span");
+
+  li.setAttribute("data-id", doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+
+  li.appendChild(name);
+  li.appendChild(city);
+
+  cafeList.appendChild(li);
+}
 // GETTING DATA
+db.collection("cafes")
+  .get()
+  .then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      render(doc);
+    });
+  });
